77772a5bfccf9f1c6f275589fb322efa26258ecc
monaSalih <monaalobisat@gmail.com>
2021-12-05 10:18:27 +0200

    ..

--- project/settings.py
+++ project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import environ
-
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -43,7 +43,7 @@ DEBUG = env.bool("DEBUG")
 
 ALLOWED_HOSTS = tuple(env.list("ALLOWED_HOSTS"))
 
-
+SIMOLE_JWT={"ACCESS_TOKEN_LIFETIME":timedelta(seconds=60*60)}
 # Application definition
 
 INSTALLED_APPS = [
